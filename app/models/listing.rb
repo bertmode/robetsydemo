21fac793afeef1d32517a201b8beb065f5ed0761
bertmode <robert@spartzinc.com>
2015-04-02 09:45:52 -0500

    config dropbox for localhost

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,12 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpng",
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpng",
+    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  else
+	  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpng",
                     :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-end
+                    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                    :path => ":style/:id_:filename"
+    end
+    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  end
