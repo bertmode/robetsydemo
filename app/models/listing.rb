8eba786238b7092da731d443be05ee051f63f098
bertmode <robert@spartzinc.com>
2015-04-07 14:02:05 -0500

    install and setup devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,13 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpng",
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpng"
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   else
 	  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpng",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                    :path => ":style/:id_:filename"
-    end
-    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
   end
+
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+end
